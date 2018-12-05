7bba2d2d2470312f9041b063df3ce0b66a7d5a48
peppocola <giuseppecolavito999@gmail.com>
2018-12-05 23:30:44 +0100

    Added comments and cleaned the code

--- Day 5/Day5.py
+++ Day 5/Day5.py
@@ -1,39 +1,34 @@
 #!/usr/bin/env python
 
 def main():
-    code=open(r"/home/peppocola/Scrivania/file").read()
+    code=open(r"/home/peppocola/Scrivania/file").read().rstrip()
     dayfivepartone(code)
     dayfiveparttwo(code)
 
 def dayfivepartone(code):
     stack=[]
-    stack.append(code[0])
-    for i in range (1, len(code)):
-        if code[i]==code[i].upper():
-            if stack and code[i].lower()==stack[-1]:
-                stack.pop()
-            else:
-                stack.append(code[i])
-        elif stack and code[i].upper()==stack[-1]:
-                stack.pop()
-        else:
-            stack.append(code[i])
+    for i in code:
+        if stack:                                                           #if stack is not empty
+            if i==i.upper():                                                #if i is uppercase
+                if i.lower()==stack[-1]:                                    #if top of stack is i lowercase
+                    stack.pop()                                             #remove
+                else:                                                       #i is uppercase but the top of the stack isnt i lowercase
+                    stack.append(i)                                         
+            elif i.upper()==stack[-1]:                                      #it's lowecase, so if i uppercase is the top of the stack
+                stack.pop()                                                 #remove
+            else:                                                           #lowercase but the top of the stack isnt i uppercase
+                stack.append(i)                                             #push
+        else:                                                               #if stack is empty
+            stack.append(i)                                                 #push
     return(len(stack))
 
 def dayfiveparttwo(code):
     alphabet='abcdefghijklmnopqrstuvwxyz'
-    dicty={}
-    for i in alphabet:
-        newcode=code.replace(i, "").replace(i.upper(), "")
-        dicty[i]=dayfivepartone(newcode)
-    print(minvalue(dicty))
-
-def minvalue(dicty):
-    miny=None
-    for key in dicty.keys():
-        if miny is None or dicty[key]<miny:
-           miny=dicty[key]
-    return miny
+    length=[]
+    for i in alphabet:                                                      #for every word in the alphabet
+        newcode=code.replace(i, "").replace(i.upper(), "")                  #remove that letter lowercase and uppercase
+        length.append(dayfivepartone(newcode))                              #put the length of the string after reactions in a list
+    print(min(length))
 
 if __name__ == '__main__':
     main()
